a0668a0a06a4ee7b40ca5b205fccbf3578527868
FuckingNoO <1158656977@qq.com>
2016-09-17 15:46:41 +0800

    stage build

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,31 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+// require('styles/App.css');
+require('../styles/App.scss');
 
 import React from 'react';
-
-let yeomanImage = require('../images/yeoman.png');
+var imageDatas = require('../images/imageData.json');
+/**
+ * 生成图片地址
+ */
+imageDatas = (function generateImageUrl(imgDataArr) {
+  for(let i = 1; i < imgDataArr.length; i++){
+    let singleimgData = imgDataArr[i];
+    singleimgData.imageUrl = require('../images/' + imgDataArr[i].filename);
+    imgDataArr[i] = singleimgData;
+  }
+  return imgDataArr;
+})(imageDatas)
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+        <section className="image-sec">
+          <image alt="" src=""/>
+        </section>
+        <nav className="controller-nav">
+        </nav>
+      </section>
     );
   }
 }
